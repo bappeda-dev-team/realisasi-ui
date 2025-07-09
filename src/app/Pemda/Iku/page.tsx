c19c587b5d7c2e08d62293487dfe710ded9d131c
RyanB1303 <ryan.brilliant.nirwana@gmail.com>
2025-07-09 09:32:28 +0700

    latest commited

---
+++ src/app/Pemda/Iku/page.tsx
@@ -0,0 +1,16 @@
+'use client'
+
+import React from 'react'
+
+const IkuPage = () => {
+  return (
+    <div className="transition-all ease-in-out duration-500">
+      <h2 className="text-lg font-semibold mb-2">Realisasi IKU Pemda</h2>
+      <p className="text-gray-700">
+        Ini adalah konten untuk halaman <strong>IKU Pemda</strong>.
+      </p>
+    </div>
+  )
+}
+
+export default IkuPage
