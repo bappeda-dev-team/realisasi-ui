25c46b0824e7a15523d6141e0d4966576e299975
RyanB1303 <ryan.brilliant.nirwana@gmail.com>
2025-07-18 10:12:35 +0700

    dummy page more

--- src/app/Opd/Sasaran/page.tsx
+++ src/app/Opd/Sasaran/page.tsx
@@ -6,7 +6,7 @@ import Table from './TableSasaran'
 const SasaranPage = () => {
   return (
     <div className="transition-all ease-in-out duration-500">
-      <h2 className="text-lg font-semibold mb-2">Realisasi Sasaran Pemda</h2>
+      <h2 className="text-lg font-semibold mb-2">Realisasi Sasaran OPD</h2>
       <Table />
     </div>
   )
