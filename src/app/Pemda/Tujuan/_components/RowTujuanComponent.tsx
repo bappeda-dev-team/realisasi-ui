3ee5bfeb67aead24987c7b87eae5ef20f822a64c
RyanB1303 <ryan.brilliant.nirwana@gmail.com>
2025-07-22 09:09:55 +0700

    hide aksi tujuan pemda if no target

--- src/app/Pemda/Tujuan/_components/RowTujuanComponent.tsx
+++ src/app/Pemda/Tujuan/_components/RowTujuanComponent.tsx
@@ -29,27 +29,31 @@ const RowTujuanComponent: React.FC<RowTujuanComponentProps> = ({
             <td className="border border-red-400 px-6 py-4 text-center">{tujuan.tujuan_pemda}</td>
             <td className="border border-red-400 px-6 py-4 text-center">{tujuan.misi}</td>
             <td className="border border-red-400 px-6 py-4 text-center">{indikator?.indikator ?? '-'}</td>
-            <td className="border border-red-400 px-6 py-4 text-center">
-                <div className="flex flex-col gap-2">
-                    <ButtonGreenBorder
-                        className="flex items-center gap-1 cursor-pointer"
-                        onClick={() => {
-                            handleOpenModal(tujuan, dataTargetRealisasi);
-                        }} >
-                        Realisasi
-                    </ButtonGreenBorder>
-                </div>
-            </td>
             <td className="border border-red-400 px-6 py-4 text-center">{indikator?.rumus_perhitungan ?? '-'}</td>
             <td className="border border-red-400 px-6 py-4 text-center">{indikator?.sumber_data ?? '-'}</td>
             {periode.map((tahun) => {
 
                 const targetData = dataTargetRealisasi.find(r => r.indikatorId === indikator.id.toString() && r.tahun === tahun.toString());
 
-                return targetData ? (
-                    <ColTargetTujuanComponent key={targetData.targetRealisasiId} target={targetData.target} satuan={targetData.satuan} realisasi={targetData.realisasi} capaian={targetData.capaian} />
+                return targetData && targetData?.target ? (
+                    <>
+                        <td className="border border-red-400 px-6 py-4 text-center">
+                            <div className="flex flex-col gap-2">
+                                <ButtonGreenBorder
+                                    className="flex items-center gap-1 cursor-pointer"
+                                    onClick={() => {
+                                        handleOpenModal(tujuan, dataTargetRealisasi);
+                                    }} >
+                                    Realisasi
+                                </ButtonGreenBorder>
+                            </div>
+                        </td>
+                        <ColTargetTujuanComponent key={targetData.targetRealisasiId} target={targetData.target} satuan={targetData.satuan} realisasi={targetData.realisasi} capaian={targetData.capaian} />
+                    </>
                 ) : (
                     <React.Fragment key={`${tujuan.id}-${tahun}`}>
+                        <td className="border border-red-400 px-6 py-4 text-center">
+                        </td>
                         <td className="border border-red-400 px-6 py-4 text-center" colSpan={4}>
                             Tidak ada target
                         </td>
--- src/app/Pemda/Tujuan/_components/TableTujuan.tsx
+++ src/app/Pemda/Tujuan/_components/TableTujuan.tsx
@@ -19,9 +19,9 @@ function TableTujuan({ periode, tujuansPemda, targetRealisasiCapaians, handleOpe
                     <th rowSpan={2} className="border-r border-b py-4 px-6 border-gray-300 min-w-[400px] text-center">Tujuan</th>
                     <th rowSpan={2} className="border-r border-b py-4 px-6 border-gray-300 min-w-[200px] text-center">Visi/Misi</th>
                     <th rowSpan={2} className="border-r border-b py-4 px-6 border-gray-300 min-w-[200px] text-center">Indikator</th>
-                    <th rowSpan={2} className="border-r border-b py-4 px-6 border-gray-300 min-w-[100px] text-center">Aksi</th>
                     <th rowSpan={2} className="border-r border-b py-4 px-6 border-gray-300 min-w-[200px] text-center">Rumus Perhitungan</th>
                     <th rowSpan={2} className="border-r border-b py-4 px-6 border-gray-300 min-w-[200px] text-center">Sumber Data</th>
+                    <th rowSpan={2} className="border-r border-b py-4 px-6 border-gray-300 min-w-[100px] text-center">Aksi</th>
                     {periode.map((tahun) => (
                         <th key={tahun} colSpan={4} className="border-r border-b py-2 px-6 border-gray-300 min-w-[100px] text-center">
                             {tahun}
