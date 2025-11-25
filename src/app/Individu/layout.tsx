4b1244e6d3c7796ceeef783b822beeb038ff7da1
RyanB1303 <ryan.brilliant.nirwana@gmail.com>
2025-11-25 08:16:19 +0700

    reorder realisasi individu

--- src/app/Individu/layout.tsx
+++ src/app/Individu/layout.tsx
@@ -16,16 +16,16 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       inactiveClass: 'text-[#1C1D1D] hover:bg-red-700 hover:text-white',
     },
     {
+      label: 'Program / Kegiatan / Sub Kegiatan',
+      href: '/Individu/Rencana-Kerja',
+      activeClass: 'bg-sky-600 text-white border-sky-600',
+      inactiveClass: 'text-[#1C1D1D] hover:bg-sky-800 hover:text-white',
+    },
+    {
       label: 'Renaksi',
       href: '/Individu/Renaksi',
       activeClass: 'bg-green-600 text-white border-green-600',
       inactiveClass: 'text-[#1C1D1D] hover:bg-green-700 hover:text-white',
-    },
-    {
-      label: 'Rencana Kerja',
-      href: '/Individu/Rencana-Kerja',
-      activeClass: 'bg-sky-600 text-white border-sky-600',
-      inactiveClass: 'text-[#1C1D1D] hover:bg-sky-800 hover:text-white',
     },
   ]
 
