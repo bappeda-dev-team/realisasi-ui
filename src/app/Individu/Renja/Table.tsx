dcd48bdacd3d0d9d2ab1a515ea88205140beccfd
Affandi Pratama <affandipratama45@gmail.com>
2026-08-02 15:47:43 +0700

    Update pesan validasi, fix bug rekin, update tampilan renja individu di setiap role

--- src/app/Individu/Renja/Table.tsx
+++ src/app/Individu/Renja/Table.tsx
@@ -2,6 +2,7 @@
 
 import React from "react";
 import { useUserContext } from "@/context/UserContext";
+import { useFilterContext } from "@/context/FilterContext";
 import { ROLES } from "@/constants/roles";
 import SasaranOpdTable from "./_components/_tables/SasaranOpdTable";
 import ProgramTable from "./_components/_tables/ProgramTable";
@@ -10,20 +11,24 @@ import SubKegiatanIndividuTable from "./_components/_tables/SubKegiatanTable";
 
 const Table = () => {
   const { user } = useUserContext();
-  const isLevel2 = user?.roles.includes(ROLES.LEVEL_2);
-  const isLevel3 = user?.roles.includes(ROLES.LEVEL_3);
+  const { activatedLevelRole } = useFilterContext();
+  const isLevel1 = activatedLevelRole === "LEVEL_1" || user?.roles.includes(ROLES.LEVEL_1);
+  const isLevel2 = activatedLevelRole === "LEVEL_2" || user?.roles.includes(ROLES.LEVEL_2);
+  const isLevel3 = activatedLevelRole === "LEVEL_3" || user?.roles.includes(ROLES.LEVEL_3);
 
   return (
     <>
       {!isLevel3 && (
         <>
-          <SasaranOpdTable />
-          <div className="mt-6">
-            <ProgramTable />
-          </div>
+          {!isLevel2 && <SasaranOpdTable />}
+          {!isLevel1 && (
+            <div className="mt-6">
+              <ProgramTable />
+            </div>
+          )}
         </>
       )}
-      {!isLevel2 && (
+      {!isLevel2 && !isLevel1 && (
         <>
           <div className="mt-6">
             <KegiatanTable />
